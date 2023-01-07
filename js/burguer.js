d85bc87a212526ae9727ce3d8f0f77c04d450f49
André Ruperto <andreruperto@gmail.com>
2023-01-07 16:04:03 -0300

    adicionando parte do homem aranha

--- js/burguer.js
+++ js/burguer.js
@@ -3,3 +3,15 @@ const menu = document.querySelector(".divi")
 const active = () => menu.classList.toggle('active')
 
 menu.addEventListener('click', active)
+
+function mudar(){
+    location.replace("./index1.html", "_self")
+}
+
+function mudar1(){
+    location.replace("./index.html", "_self")
+}
+
+function voltar(){
+    location.replace("./projeto.html", "_self")
+}
